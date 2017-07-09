9d70d01eaf0489136fafe348518917785b8eb712
Tetsuo Seto <tseto@us.ibm.com>
2017-07-08 20:59:57 -0700

    Version 1

--- src/application.ts
+++ src/application.ts
@@ -1,9 +1,17 @@
 import {Application} from '@loopback/core';
+import {Logger, LoggingOptions} from '@loopback/logging';
 import {HelloWorldComponent} from './components/hello-world';
 
-export function createApp(): Application {
-    return new Application({
-        components: [HelloWorldComponent],
+export class HelloWorldApplication extends Application {
+  private logger: Logger;
+  constructor() {
+    super({
+      components: [HelloWorldComponent],
     });
+    const options: LoggingOptions = { http: true };
+    this.logger = new Logger(this, options);
+  }
+  get log() {
+    return this.logger.log;
+  }  
 }
-
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import {createApp} from './application';
+import {HelloWorldApplication} from './application';
 import {HelloWorldController} from './components/hello-world/controllers/hello-world';
 import * as http from 'http';
 
-const app = createApp();
+const app = new HelloWorldApplication();
 app.controller(HelloWorldController);
 
 const port = 3000;
 const server = http.createServer(app.handleHttp);
 server.listen(3000, (err) => {
-  if (err) throw err;
+  if (err) {
+    app.log.error(err);
+    throw err;
+  }
+  app.log.info(`*** HTTP server started on port ${3000}`);
   console.log(`HTTP server listening on port ${3000}`);
   console.log('Run `curl localhost:3000/helloworld?name=YOUR_NAME` to try it out');
 });
